abe4006a7b4390c59fdf00c9c1d6121b075d466d
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2022-01-24 03:44:09 +0000

    v1.0

--- client.py
+++ client.py
@@ -3,23 +3,27 @@ import socket
 import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("192.168.42.129",9090))
+client.connect(("127.0.0.1",9090))
 client.send("Ready".encode())
 print("Sending ack")
 while True:
-    
-    ref_hash = client.recv(1024)
-    print("ref hash: ", ref_hash)
-    t1 = time.time()
-    for x in range(100000):
-        num = str(x).encode()
-        my_hash = hashlib.sha256(num).hexdigest().encode()
-        if(my_hash == ref_hash):
-            t2 = time.time()
-            t = format((t2- t1)*1000, ".3f")
-            client.send(f'Found hash: {x}'.encode())
-            ref_hash = ""
-            print(f'found hash in {t}ms')
-            reward = client.recv(1024)
-            print(reward.decode())
+ ref_hash = client.recv(1024)
+ print("Job: ", ref_hash)
+ diff = client.recv(1024)
+ print("Difficulty: ",diff.decode())
+ t1 = time.time()
+ for x in range(100000):
+  num = str(x).encode()
+  my_hash = hashlib.sha256(num).hexdigest().encode()
+  if(my_hash == ref_hash):
+   t2 = time.time()
+   tim = format((t2- t1)*1000, ".3f")
+   t_taken = float(tim)
+   hashrate = format(x/(t_taken/1000), ".0f")
+   client.send(f'Found hash: {x}'.encode())
+   ref_hash = ""
+   print(f'found hash in {tim}ms')
+   print(f'Hashrate: {hashrate}H/s')
+   reward = client.recv(1024)
+   print(reward.decode())
 
--- server.py
+++ server.py
@@ -4,10 +4,13 @@ import random
 import socket
 import time
 
+# Initialize variables
 hash = ""
+diff = "100000"
+
 # Start the the sever and listen for the client
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("192.168.42.48",9090))
+server.bind(("127.0.0.1",9090))
 server.listen()
 
 # Function for generating hash
@@ -16,25 +19,25 @@ def gen_hash(hash):
     Digit = str(a).encode()
     hash = hashlib.sha256(Digit).hexdigest()
     return hash
-hashb = gen_hash(hash)
-print(hashb)
+
 (clientConnected, clientAddress) = server.accept()
 print("Accepted a connection request from %s:%s"%(clientAddress[0], clientAddress[1]))
 s = clientConnected.recv(1024)
 stats = s.decode()
-print(stats)
 if(stats == "Ready"):
     print("Recived ack, sending job")
-    
+    hashb = gen_hash(hash)
     print(hashb)
     clientConnected.send(hashb.encode())
+    time.sleep(1)
+    clientConnected.send(diff.encode())
     while(True):
         hash_stats = clientConnected.recv(1024).decode()
         if("Found hash: " in hash_stats):
             num = hash_stats.split("Found hash: ")
             cnum1 = num[1].replace("b'", "")
             cnum = cnum1.replace("'", "")
-            print(cnum)
+            print(num[1])
             if(cnum == hashb):
                 print("Wrong hash returned")
                 clientConnected.send("BAD SHARES".encode())
@@ -49,6 +52,8 @@ if(stats == "Ready"):
                     hashb = gen_hash(hash)
                     time.sleep(5)
                     clientConnected.send(hashb.encode())
+                    time.sleep(3)
+                    clientConnected.send(str(diff).encode())
                     print(f'Sending job: {hashb}')
 
             
